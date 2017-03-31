c3837f312bba7e8655fd379fc500676bbea58b3f
Tống Hữu Lộc <loctong@executionlab.asia>
2017-03-31 18:29:44 +0700

    move folder for midterm review, complete main layout

---
+++ Mid-Review/assets/js/main.js
@@ -0,0 +1,148 @@
+$(function() {
+    var owlMen = $('#men-carousel');
+    owlMen.owlCarousel({
+        items: 1,
+        loop: true,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        animateIn: 'fadeInLeft',
+        animateOut: 'fadeOutRight',
+        smartSpeed: 800
+    });
+    var owlWomen = $('#women-carousel');
+    owlWomen.owlCarousel({
+        items: 1,
+        loop: true,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        animateIn: 'fadeInRight',
+        animateOut: 'fadeOutLeft',
+        smartSpeed: 800
+    });
+
+    // navbar scroll down to hide
+    $('html').on('DOMMouseScroll mousewheel', function(e) {
+        if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) { //alternative options for wheelData: wheelDeltaX & wheelDeltaY
+            //scroll down
+            $(".navbar").addClass("hide-nav-bar");
+        } else {
+            //scroll up
+            $(".navbar").removeClass("hide-nav-bar");
+        }
+        //prevent page fom scrolling
+        //return false;
+    });
+
+    // search btn slide toggle
+    var sliBtn = '.search-btn',
+        sliCont = '.search-slide',
+        sliTxt = '.search-slide input[type=text]',
+        sliDis = '.search-close',
+        sliSpd = 300;
+
+    $(sliBtn).click(function() {
+        $(sliCont).animate({ 'width': '15.5625em' }, sliSpd);
+        $(sliTxt).focus();
+    });
+    $(sliDis).click(function() {
+        $(sliCont).animate({ 'width': 0 }, sliSpd);
+    });
+
+    // toggle slide cart
+    var $shadow_layer = $('#cd-shadow-layer'),
+        $lateral_cart = $('#cd-cart'),
+        $cart_trigger = $('#cd-cart-trigger');
+
+    //open cart
+    $cart_trigger.on('click', function(event) {
+        event.preventDefault();
+        $lateral_cart.addClass('speed-in');
+        $shadow_layer.css({
+            "opacity": 1,
+            "z-index": 2
+        });
+    });
+
+    //close lateral cart or lateral menu
+    $shadow_layer.on('click', function() {
+        $lateral_cart.removeClass('speed-in');
+        $shadow_layer.css("opacity", "0");
+        setTimeout(function() {
+            $shadow_layer.css("z-index", "0");
+        }, 500)
+    });
+
+    // menu sidebar click toggle
+    $(".navToggle").on("click", function() {
+        $(this).toggleClass("open");
+        $("#goof-menu").toggleClass("active");
+        if ($("#goof-menu").hasClass("active")) {
+            $(".sidebar").css("right", "150%");
+        } else {
+            $(".sidebar").css("right", "0%");
+        }
+    })
+
+    //sort filter menu
+    var $typeList = $('#filter-type'),
+        $typeListItem = $typeList.children('li');
+
+    $typeListItem.sort(function(a, b) {
+        var an = a.innerText,
+            bn = b.innerText;
+        if (an == "All" || bn == "All") { return 1; }
+        if (an > bn) {
+            return 1;
+        }
+        if (an < bn) {
+            return -1;
+        }
+        return 0;
+    });
+
+    $typeListItem.detach().appendTo($typeList);
+
+    // change img on hover
+    var img_path = "../../assets/img/products/jacket/thumb_goof_product_jacket_";
+    $(".img-block img").on("mouseenter", function() {
+        var $el = $(this),
+            main_src = $(this).attr("src"),
+            sub_src = main_src.replace("1.png", "2.png");
+        $el.attr("src", sub_src);
+    });
+    $(".img-block img").on("mouseout", function() {
+        var $el = $(this),
+            main_src = $(this).attr("src"),
+            sub_src = main_src.replace("2.png", "1.png");
+        $el.attr("src", sub_src);
+    });
+
+    // user edit
+    var chosen_row;
+    $(".edit-address").on("click", function() {
+        chosen_row = $(this).parent().parent();
+        chosen_row.hide();
+        $("#row-form-address").show();
+        $("#form-address").css("opacity", "1");
+    });
+
+    // typing animation
+    (function($) {
+        $.fn.writeText = function(content) {
+            var contentArray = content.split(""),
+                current = 0,
+                elem = this;
+            setInterval(function() {
+                if (current < contentArray.length) {
+                    elem.text(elem.text() + contentArray[current++]);
+                }
+            }, 80);
+        };
+
+    })(jQuery);
+
+    // input text for typing animation 
+    $("#holder").writeText("HIGH-END FAHSION / LUXURY STREET WEAR");
+})
---
+++ Mid-Review/config/compass.rb
@@ -0,0 +1,27 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+
+http_path = "/"
+css_dir = "./assets/css"
+css_path = "./assets/css"
+sass_dir = "./assets/sass"
+sass_path = "./assets/sass"
+images_dir = "img"
+images_path = "./assets/img"
+javascripts_dir = "javascripts"
+javascripts_path = "./assets/js"
+
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass ./assets/sass scss && rm -rf sass && mv scss sass
