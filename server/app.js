5915f6ec7492a6b914ae163f8d95ec36c5595349
Tống Hữu Lộc <loctong@executionlab.asia>
2017-04-23 11:44:59 +0700

    remove redundant

--- server/app.js
+++ server/app.js
@@ -24,6 +24,18 @@ var express = require('express'),
     app = express(),
     db = require('./models');
 
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+    if (err) throw err;
+    console.log('Connected to postgres! Getting schemas...');
+
+    client
+        .query('SELECT table_schema,table_name FROM information_schema.tables;')
+        .on('row', function(row) {
+            console.log(JSON.stringify(row));
+        });
+});
+
 //assign pug engine to pug files
 app.engine('pug', cons.pug);
 
