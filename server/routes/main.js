4bc197277231f4db06c1cd4b5a53749681924a95
Tống Hữu Lộc <loctong@executionlab.asia>
2017-04-25 05:39:58 +0700

    fix bug

--- server/routes/main.js
+++ server/routes/main.js
@@ -9,9 +9,9 @@ exports.shop_men = function(req, res) {
     // var img_list = app.dirTree(app.img_path);
     db.products.findAll().then(function(products) {
         if (req.user) {
-            res.render("pages/shop", { user: req.user.email, products_list: products, title: 'G-O-O-F / MEN' });
+            res.render("pages/shop", { user: req.user.email, products: products, title: 'G-O-O-F / MEN' });
         } else {
-            res.render("pages/shop", { user: "", products_list: products, title: 'G-O-O-F / MEN' });
+            res.render("pages/shop", { user: "", products: products, title: 'G-O-O-F / MEN' });
         }
     })
 
