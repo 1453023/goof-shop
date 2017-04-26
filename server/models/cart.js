9417f211f688eb538835e4ad33bb78bda8e9a6f2
Tống Hữu Lộc <loctong@executionlab.asia>
2017-04-26 19:19:55 +0700

    fix bugs

--- server/models/cart.js
+++ server/models/cart.js
@@ -7,7 +7,7 @@ module.exports = function(sequelize, DataTypes) {
         classMethods: {
             associate: function(models) {
                 // associations can be defined here
-                cart.belongsTo(models.products, { foreignKey: 'product_id' });
+                cart.belongsTo(models.products, { foreignkey: 'product_id' })
             }
         }
     });
